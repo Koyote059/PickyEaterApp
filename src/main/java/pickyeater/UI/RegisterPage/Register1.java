fa6d3c9dadd8ae50ad9fcf833af5c1dc58926883
kaeong <koyote059@gmail.com>
2022-05-05 17:49:59 +0200

    UI

--- src/main/java/pickyeater/UI/RegisterPage/Register1.java
+++ src/main/java/pickyeater/UI/RegisterPage/Register1.java
@@ -134,6 +134,9 @@ public class Register1 extends JFrame{
                 if (nameTmp != null && sexTmp != null && weightTmp != 0 && heightTmp != 0 && birthDayTmp != null){
                     JOptionPane.showMessageDialog(panelZeroOne, "Selected:"  + "\n" +  "Name: " + nameTmp + "\n" + "Weight: " + weightTmp + "Kg\n" + "Height: " + heightTmp + "cm\n" + "Birthday: " + birthDayTmp + "\n" + "Sex: " + sexTmp + "\n" + "Body fat: " + bodyFatTmp + "%");
                     System.out.println("OK!");
+
+                    setVisible(false);
+                    EventQueue.invokeLater(Register2::new);
                 }
             }
         });
--- src/main/java/pickyeater/UI/RegisterPage/Register2.java
+++ src/main/java/pickyeater/UI/RegisterPage/Register2.java
@@ -1,4 +1,19 @@
 package pickyeater.UI.RegisterPage;
 
-public class Register2 {
+import javax.swing.*;
+
+public class Register2 extends JFrame {
+    private JPanel panel1;
+    private JButton sedentaryButton;
+    private JButton veryActiveButton;
+    private JButton slightlyActiveButton;
+    private JButton activeButton;
+    private JPanel mainPanel;
+
+    public Register2() {
+        setContentPane(mainPanel);
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
