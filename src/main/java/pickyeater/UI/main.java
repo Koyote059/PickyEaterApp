a98f78ba7479f440235816c118d424e887b1b548
ZiCli <claudiodimaio01@gmail.com>
2022-05-13 08:25:28 +0200

    N/A

--- src/main/java/pickyeater/UI/main.java
+++ src/main/java/pickyeater/UI/main.java
@@ -6,6 +6,8 @@ package pickyeater.UI;
 import pickyeater.UI.leftbuttons.MainButton;
 import pickyeater.UI.leftbuttons.PanelButtons;
 import pickyeater.UI.registerpage.Register1;
+import pickyeater.basics.food.PickyIngredient;
+import pickyeater.builders.PickyIngredientBuilder;
 import pickyeater.database.*;
 import pickyeater.executors.ExecutorProvider;
 import pickyeater.managers.EaterManager;
@@ -22,6 +24,8 @@ public class main {
                 pickyEatersDB.getMealsDatabase());
 
         ExecutorProvider executorProvider = new ExecutorProvider(eaterManager);
+
+        //eaterManager.getFoodManager().saveIngredient(new PickyIngredient());
 
         if (eaterManager.getUserManager().getUser().isEmpty()) {  // User Database is empty
             new Register1(eaterManager, executorProvider);
