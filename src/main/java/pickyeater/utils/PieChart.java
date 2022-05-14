71496e7cc3cdf7ba1f1ff16dce967d558e61e4c9
ZiCli <claudiodimaio01@gmail.com>
2022-05-14 11:28:01 +0200

    Added PieChart.java to utils

---
+++ src/main/java/pickyeater/utils/PieChart.java
@@ -0,0 +1,54 @@
+package pickyeater.utils;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Slice {
+    double value;
+    Color color;
+
+    public Slice(double value, Color color) {
+        this.value = value;
+        this.color = color;
+    }
+}
+
+class MyComponent extends JComponent {
+    Slice[] slices = { new Slice(5, Color.black), new Slice(33, Color.green),
+            new Slice(20, Color.yellow), new Slice(15, Color.red) };
+
+    MyComponent() {
+
+    }
+    public void paint(Graphics g) {
+        drawPie((Graphics2D) g, getBounds(), slices);
+    }
+
+    void drawPie(Graphics2D g, Rectangle area, Slice[] slices) {
+        double total = 0.0D;
+        for (int i = 0; i < slices.length; i++) {
+            total += slices[i].value;
+        }
+
+        double curValue = 0.0D;
+        int startAngle = 0;
+        for (int i = 0; i < slices.length; i++) {
+            startAngle = (int) (curValue * 360 / total);
+            int arcAngle = (int) (slices[i].value * 360 / total);
+
+            g.setColor(slices[i].color);
+            g.fillArc(area.x, area.y, area.width, area.height, startAngle, arcAngle);
+            curValue += slices[i].value;
+        }
+    }
+}
+
+public class PieChart {
+    public static void main(String[] argv) {
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(new MyComponent());
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+
+    }
+}
