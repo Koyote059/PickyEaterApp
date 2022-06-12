9ad3e28e578f3325c3de4715b0b7390d7d589a83
ZiCli <claudiodimaio01@gmail.com>
2022-06-12 10:57:17 +0200

    minor change

--- src/main/java/pickyeater/ApplicationMain.java
+++ src/main/java/pickyeater/ApplicationMain.java
@@ -13,13 +13,12 @@ import pickyeater.utils.Resources;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ResourceBundle;
 
 public class ApplicationMain {
     public static void main(String[] args) {
         ThemeHandler.ReadTheme();
 
-        //PickyEatersDatabase pickyEatersDB = new SQLPickyEaterDB("Resources.getDatabase()");
+        //PickyEatersDatabase pickyEatersDB = new SQLPickyEaterDB(Resources.getDatabase());
         PickyEatersDatabase pickyEatersDB = new SQLPickyEaterDB("DatabasePickEaterCopy.sqlite");
         EaterManager eaterManager = new PickyEaterManager(pickyEatersDB.getUserDatabase(), pickyEatersDB.getIngredientsDatabase(), pickyEatersDB.getMealsDatabase(), pickyEatersDB.getGroceriesDatabase());
         ExecutorProvider.setEaterManager(eaterManager);
